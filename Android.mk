b19665c39da76c0e24c8cd9583e30c4a50567510
rpcraig <rpcraig@tycho.ncsc.mil>
2012-07-30 09:33:03 -0400

    Add mac_permissions.xml file.
    This was moved from external/mac-policy.git

--- Android.mk
+++ Android.mk
@@ -101,5 +101,17 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 ##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mac_permissions.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/security
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
+
+##################################
 
 endif #ifeq ($(HAVE_SELINUX),true)
