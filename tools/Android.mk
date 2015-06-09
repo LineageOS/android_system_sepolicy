53b480137fd360676df88517b481cf1e04f66da6
Jeff Vander Stoep <jeffv@google.com>
2015-06-09 12:53:12 -0700

    tools: use headers from common selinux project
    Point to external/selinux/libsepol instead of external/libsepol
    
    Change-Id: If6dc1e9261f397d801ba2376ab60c5dc5b5d86e4

--- tools/Android.mk
+++ tools/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := checkseapp
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := external/libsepol/include/
+LOCAL_C_INCLUDES := external/selinux/libsepol/include/
 LOCAL_CFLAGS := -DLINK_SEPOL_STATIC -Wall -Werror
 LOCAL_SRC_FILES := check_seapp.c
 LOCAL_STATIC_LIBRARIES := libsepol
@@ -17,7 +17,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := checkfc
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := external/libsepol/include \
+LOCAL_C_INCLUDES := external/selinux/libsepol/include \
                     external/libselinux/include
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_SRC_FILES := checkfc.c
@@ -41,7 +41,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := sepolicy-check
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := external/libsepol/include
+LOCAL_C_INCLUDES := external/selinux/libsepol/include
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_SRC_FILES := sepolicy-check.c
 LOCAL_STATIC_LIBRARIES := libsepol
