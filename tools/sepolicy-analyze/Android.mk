73f7e76837c1f3d6a1360d2d4a06c9037d7addc1
dcashman <dcashman@google.com>
2015-06-10 10:42:41 -0700

    sepolicy-analyze: use headers from common selinux project.
    Point to external/selinux/libsepol instead of external/libsepol.
    
    (cherry picked from commit 96136d847d24ceff72c9a401717fb99366003498)
    
    Change-Id: I09c33a4cbd7b4cd3ef2341c042259b96c0b59372

--- tools/sepolicy-analyze/Android.mk
+++ tools/sepolicy-analyze/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := sepolicy-analyze
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := external/libsepol/include
+LOCAL_C_INCLUDES := external/selinux/libsepol/include
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_SRC_FILES := sepolicy-analyze.c dups.c neverallow.c perm.c typecmp.c booleans.c attribute.c utils.c
 LOCAL_STATIC_LIBRARIES := libsepol
