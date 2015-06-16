bce406f7e1ea07d3eaea8eab9d3999e05d57cfce
Ricardo Cerqueira <ricardo@cyngn.com>
2015-06-16 02:17:56 +0100

    domain: Restore original neverallow rules for shipping builds
    MR1 CTS validates the integrity of neverallow rules, so we can't
    change them
    
    Fixes android.cts.security.SELinuxNeverallowRulesTest#testNeverallowRules82
    
    Change-Id: If8d3e26355a0774edee60ac9293a58eb48dc1b48

--- Android.mk
+++ Android.mk
@@ -95,6 +95,7 @@ $(sepolicy_policy.conf): $(call build_policy, $(sepolicy_build_files))
 	$(hide) m4 $(PRIVATE_ADDITIONAL_M4DEFS) \
 		-D mls_num_sens=$(PRIVATE_MLS_SENS) -D mls_num_cats=$(PRIVATE_MLS_CATS) \
 		-D target_build_variant=$(TARGET_BUILD_VARIANT) \
+		-D shipping_build=$(CYNGN_TARGET) \
 		-s $^ > $@
 	$(hide) sed '/dontaudit/d' $@ > $@.dontaudit
 
