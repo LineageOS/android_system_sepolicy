8e1d2ee0adc33cca13a6e54f94a6a028fa321ff7
LuK1337 <priv.luk@gmail.com>
2016-09-18 10:48:17 +0200

    sepolicy: Allow optional platform text relocations
    [AdrianDC]
      * Add 'target_needs_platform_text_relocations'
         variable to each sepolicy build rules needing it
         to avoid mismatching domain.te uses on AOSP O
    
      * Port to Android 8.1 with unique definitions.mk
    
    Change-Id: Ibcf8a1014c8fcfdc33ea26e1e852bc5f58d20af6
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- definitions.mk
+++ definitions.mk
@@ -10,6 +10,7 @@ $(hide) m4 $(PRIVATE_ADDITIONAL_M4DEFS) \
 	-D target_with_asan=$(PRIVATE_TGT_WITH_ASAN) \
 	-D target_full_treble=$(PRIVATE_SEPOLICY_SPLIT) \
 	-D target_compatible_property=$(PRIVATE_COMPATIBLE_PROPERTY) \
+	-D target_needs_platform_text_relocations=$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS) \
 	$(PRIVATE_TGT_RECOVERY) \
 	-s $^ > $@
 endef
