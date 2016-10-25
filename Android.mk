3469c8338844eb98e66dea72b403dc03f758197c
LuK1337 <priv.luk@gmail.com>
2016-10-25 23:26:12 +0200

    Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
    Change-Id: Ica4926fa3acf88b95c687f25eec4dbc68ea41fbb

--- Android.mk
+++ Android.mk
@@ -431,6 +431,7 @@ $(PLAT_PUBLIC_POLICY) $(REQD_MASK_POLICY) $(PLAT_VENDOR_POLICY) $(BOARD_SEPOLICY
 		-D target_arch=$(PRIVATE_TGT_ARCH) \
 		-D target_with_asan=$(PRIVATE_TGT_WITH_ASAN) \
 		-D target_full_treble=$(PRODUCT_FULL_TREBLE) \
+		-D target_has_legacy_camera_hal1=$(TARGET_HAS_LEGACY_CAMERA_HAL1) \
 		-D target_needs_platform_text_relocations=$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS) \
 		-s $^ > $@
 	$(hide) sed '/dontaudit/d' $@ > $@.dontaudit
