0b977bcddccec60c82767e73f8b95e09f8ebbcc9
Michael Bestas <mkbestas@lineageos.org>
2022-03-09 17:53:53 +0200

    [SQUASH]
    Change-Id: I4a58c193ad17d21253f84001a30418d517e6247c

--- Android.mk
+++ Android.mk
@@ -1517,6 +1517,8 @@ version_under_treble_tests := 29.0
 include $(LOCAL_PATH)/treble_sepolicy_tests_for_release.mk
 version_under_treble_tests := 30.0
 include $(LOCAL_PATH)/treble_sepolicy_tests_for_release.mk
+version_under_treble_tests := 31.0
+include $(LOCAL_PATH)/treble_sepolicy_tests_for_release.mk
 endif  # PRODUCT_SEPOLICY_SPLIT
 
 version_under_treble_tests := 26.0
@@ -1528,6 +1530,8 @@ include $(LOCAL_PATH)/compat.mk
 version_under_treble_tests := 29.0
 include $(LOCAL_PATH)/compat.mk
 version_under_treble_tests := 30.0
+include $(LOCAL_PATH)/compat.mk
+version_under_treble_tests := 31.0
 include $(LOCAL_PATH)/compat.mk
 
 base_plat_policy.conf :=
