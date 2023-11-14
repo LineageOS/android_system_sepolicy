b9517900e6224afc040ba57e32aabfb8d559721c
Jooyung Han <jooyung@google.com>
2023-11-14 13:50:14 +0900

    apex_sepolicy_tests: check apex roots are search-able
    Narrow down the check for apex roots. It was 'read', but 'search' should
    be enough.
    
    Bug: 310528686
    Test: m
    Change-Id: Ibe5f2e948464580832d87e8d8364c33a437efed2

--- tests/apex_sepolicy_tests_test.py
+++ tests/apex_sepolicy_tests_test.py
@@ -96,7 +96,7 @@ class ApexSepolicyTests(unittest.TestCase):
         self.assert_error('./etc/linker.config.pb u:object_r:vendor_file:s0',
                         r'Error: .*linkerconfig.* can\'t read')
         self.assert_error('./ u:object_r:apex_data_file:s0',
-                        r'Error: .*linkerconfig.* can\'t read')
+                        r'Error: .*linkerconfig.* can\'t search')
 
     def test_unknown_label(self):
         self.assert_error('./bin/hw/foo u:object_r:foo_exec:s0',
